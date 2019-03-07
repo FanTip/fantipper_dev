299c06dc58e488222c84880f2e0ed247eb258d2b
Malith077 <100671040@student.swin.edu.au>
2019-03-07 14:59:23 +1100

    patched

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ app.use(function(err, req, res, next) {
 
 mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect('mongodb://localhost:27017/fantipper');
-let db = mongoose.connection;
+var db = mongoose.connection;
 db.once('open', function(){
   console.log('Connection Successful');
 });
