aded8e97975bed55142aad4376c8364ffa363eea
Malith077 <malith.git@gmail.com>
2019-11-27 22:40:24 +1100

    added images to the messages

--- public/javascripts/message_handler.js
+++ public/javascripts/message_handler.js
@@ -13,13 +13,14 @@ $(document).ready(function() {
         let messages_append = $('#total_messages');
         response.num_of_messages > 0 ? messages_append.append(response.num_of_messages) : messages_append.append('0');
         let base = $('#message_base');
-
         for (let i = 0; i < response.messages.length; i++) {
-            if (response.messages[i].length > 0) {
+            if (response.messages[i].message.length > 0) {
                 let card = $('<div class="card" style="width: 100%;">');
                 let card_body = $('<div class="card-body" style="padding: 0.5rem;">');
                 let card_content = $('<p class="card-text">');
-                card_content.append(response.messages[i]);
+                let image = $("<img src=" + response.messages[i].image + " height='50' width='50' class='rounded-circle' />")
+                card_content.append(response.messages[i].message);
+                card_body.append(image);
                 card_body.append(card_content);
                 card.append(card_body);
                 base.append(card);
--- routes/api/get_tips.js
+++ routes/api/get_tips.js
@@ -3,51 +3,52 @@ const router = express.Router();
 const User = require('../../models/user');
 const Tips = require('../../models/tips');
 
-router.get('/:url/:isMessages', async function(req, res) {
-    try{
-        
-        let user_url = req.params.url;
-        let is_messages = req.params.isMessages;
+router.get('/:url/:isMessages', async function (req, res) {
+  try {
 
-        let search_query = {
-            'creator.creatorUrl' : user_url
-        };
+    let user_url = req.params.url;
+    let is_messages = req.params.isMessages;
 
-        let user = await User.findOne(search_query).exec();
+    let search_query = {
+      'creator.creatorUrl': user_url
+    };
 
-        let tips = await Tips.find({'id' : user._id});
+    let user = await User.findOne(search_query).exec();
 
-        let response;
-        
-        if(is_messages === 'true')
-        {
-            let messages =[];
-            for(let i = 0; i < tips.length; i++)
-            {
-                if(tips[i].tipmessage.length > 0)
-                {
-                    messages.push(tips[i].tipmessage);
-                }
-                
-            }
-            response = {
-                num_of_messages : messages.length,
-                messages : messages
-            }
+    let tips = await Tips.find({
+      'id': user._id
+    });
+
+    let response;
+
+    if (is_messages === 'true') {
+      let messages = [];
+      let pic;
+      for (let i = 0; i < tips.length; i++) {
+        let paid_user = await User.findOne({'email' : tips[i].pay_email}).exec();
+        paid_user != null ? pic = paid_user.imagepath : 'null';
+        if (tips[i].tipmessage.length > 0) {
+          messages.push({
+            'message' : tips[i].tipmessage,
+            'image' : pic
+          });          
         }
-        else
-        {
-            response = {
-                num_of_tips: tips.length,
-                tips_data : tips
-            }
-        }
-        res.status(200).json(response);
+
+      }
+      response = {
+        num_of_messages: messages.length,
+        messages: messages,
+      }
+    } else {
+      response = {
+        num_of_tips: tips.length,
+        tips_data: tips
+      }
     }
-    catch(e)
-    {
-        res.status(500).send(e);        
-    }
+    res.status(200).json(response);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 
