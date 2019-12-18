72b9c99d7887749d52b3dd3118210eb42f344306
Malith077 <malith.git@gmail.com>
2019-12-18 22:43:06 +1100

    Changed spinner

--- public/javascripts/stripe_save_card.js
+++ public/javascripts/stripe_save_card.js
@@ -206,12 +206,12 @@ let getPublicKey = function () {
 let changeLoadingState = function (isLoading) {
   if (isLoading) {
     document.querySelector("button").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
+    // document.querySelector("#spinner").classList.remove("hidden");
+    // document.querySelector("#button-text").classList.add("hidden");
   } else {
     document.querySelector("button").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
+    // document.querySelector("#spinner").classList.add("hidden");
+    // document.querySelector("#button-text").classList.remove("hidden");
   }
 };
 
