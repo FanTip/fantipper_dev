9de00952afe2fb839a4cb2f94fe17f83381e7a4f
Malith077 <100671040@student.swin.edu.au>
2019-12-31 13:43:12 +1100

    added Fan tip history

--- routes/api/get_set_categories.js
+++ routes/api/get_set_categories.js
@@ -1,12 +1,16 @@
-
 const express = require('express');
 const router = express.Router();
 const User = require("../../models/user");
 
-router.get('/get', async function(req, res){
-    let user = await User.findById(req.user._id).exec();
-    let categories = user.creator.creatorCategories;
-    res.status(200).json(categories);
+router.get('/get', async function(req, res) {
+    try {
+        let user = await User.findById(req.user._id).exec();
+        let categories = user.creator.creatorCategories;
+        res.status(200).json(categories);
+    } catch (e) {
+        res.status(500).send({});
+    }
+
 });
 
 module.exports = router;
