6692d9917227ff45fdba7574a3e6a27073cc00e2
M@lith077 <malith.git@gmail.com>
2020-06-04 18:33:11 +1000

    tipping done

--- public/javascripts/sources/generic_javascript.js
+++ public/javascripts/sources/generic_javascript.js
@@ -5,7 +5,7 @@ function getYear() {
 function copyWrite() {
     var year = getYear()
     var copyWrite = document.getElementById('footer_c');
-    copyWrite.innerHTML = '© Copyright 2018 - ' + year + '<a href="https://fantipper.com"> ' +  window.location.href + '</a>';
+    copyWrite.innerHTML = '© Copyright 2018 - ' + year + '<a href="https://fantipper.com.au"> fantipper.com.au</a>';
 }
 
 $(window).on('load', function() {
